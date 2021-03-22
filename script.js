d5c16ca4b7ce79c7220870d75121228d3f127819
Fernando Rios-Avila <f.rios.a@gmail.com>
2021-03-22 09:42:15 -0400

    fase2

---
+++ script.js
@@ -0,0 +1,46 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+$(document).ready(function(){
+	$(".contentsPanel").each(function() {                
+		$(this).prepend('<div class="hidePanel">[hide]</div><div class="showPanel">[show]</div>');
+	});
+	
+	
+    $(".hidePanel").click(function(){
+		$( this ).siblings('ul').hide( 150, function() { 
+			$(this).parent().addClass('minimizedPanel');
+		});
+    });
+    $(".showPanel").click(function(){
+		$( this ).siblings('ul').show( 150, function() { 
+			$(this).parent().removeClass('minimizedPanel');
+		});
+    });
+	
+	
+});
+
+
